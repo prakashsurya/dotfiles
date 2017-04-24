1cd1717d4b6d2a612b72c3d6a312b74cb282c08f
Prakash Surya <prakash.surya@delphix.com>
2017-04-24 12:14:26 -0700

    bash: add "~/.local/bin" to PATH

--- ansible/roles/dot-bash/files/home/user/.bashrc.d/50-delphix.bash
+++ ansible/roles/dot-bash/files/home/user/.bashrc.d/50-delphix.bash
@@ -6,11 +6,14 @@ export SWT_GTK3=0
 #
 # Add Delphix specific paths if they exist:
 #
+# * ~/.local/bin is used by "pip install --user"
+#
 # * ~/bin/dlpx contains various one-off scripts to ease interaction with
 #   Delphix infrastructure.
 #
 # * ~/dev/dlpx-git-utils holds Delphix software development utilities.
 #
+[ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
 [ -d "$HOME/bin/dlpx" ] && export PATH="$HOME/bin/dlpx:$PATH"
 [ -d "$HOME/dev/dlpx-git-utils" ] && \
        export PATH="$HOME/dev/dlpx-git-utils/bin:$PATH"
