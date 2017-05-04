1e4dcb4bbfe78ccf903dfd8cea4ea417cc4eb1e1
Prakash Surya <prakash.surya@delphix.com>
2017-05-04 10:23:04 -0700

    bash: use new location for git-utils directory

--- ansible/roles/dot-bash/files/home/user/.bashrc.d/50-delphix.bash
+++ ansible/roles/dot-bash/files/home/user/.bashrc.d/50-delphix.bash
@@ -15,5 +15,5 @@ export SWT_GTK3=0
 #
 [ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
 [ -d "$HOME/bin/dlpx" ] && export PATH="$HOME/bin/dlpx:$PATH"
-[ -d "$HOME/dev/dlpx-git-utils" ] && \
-       export PATH="$HOME/dev/dlpx-git-utils/bin:$PATH"
+[ -d "$HOME/projects/git-utils" ] && \
+       export PATH="$HOME/projects/git-utils/bin:$PATH"
